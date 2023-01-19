2d3c6300b24cdb8e88988248907b57c5c7e02054
DiegoMD4 <diegolink4@hotmail.com>
2023-01-19 13:43:20 -0600

    19/1

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -89,7 +89,7 @@
         <div id="page-content-wrapper">
 
         <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom navbar-light navbar-dark bg-dark">
-            <button class="btn btn-primary" id="menu-toggle"> <-Menu</button>
+            <button class="btn btn-primary" id="menu-toggle">Menu</button>
 
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
