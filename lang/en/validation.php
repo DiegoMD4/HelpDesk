7be1d82224579d25d3c6a1517c550bef21715e30
DiegoMD4 <diegolink4@hotmail.com>
2023-01-24 19:46:33 -0600

    .

--- lang/en/validation.php
+++ lang/en/validation.php
@@ -136,7 +136,7 @@ return [
     'starts_with' => 'The :attribute must start with one of the following: :values.',
     'string' => 'The :attribute must be a string.',
     'timezone' => 'The :attribute must be a valid timezone.',
-    'unique' => 'The :attribute has already been taken.',
+    'unique' => ' :attribute ya está en uso.',
     'uploaded' => 'The :attribute failed to upload.',
     'url' => 'The :attribute must be a valid URL.',
     'uuid' => 'The :attribute must be a valid UUID.',
