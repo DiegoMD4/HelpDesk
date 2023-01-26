878228d23366b65d5506d3e155f33154aacdfcda
DiegoMD4 <diegolink4@hotmail.com>
2023-01-25 18:59:20 -0600

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,17 +72,19 @@
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
-
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    @if(Auth()->check() && auth()->user()->role == 'admin')
+                                    
+                                    <a class="dropdown-item" href="{{url('/admin')}}">
+                                        {{ __('Panel de Administrador') }}
+                                    </a>
+                                    @endif
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Cerrar Sesión') }}
                                     </a>
-                                    <a class="dropdown-item" href="{{url('/admin')}}">
-                                        {{ __('Panel de Administrador') }}
-                                    </a>
-
+                                    
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
