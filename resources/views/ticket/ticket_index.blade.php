52ff8d077af83c0708870eac8fab0d86e8f2b841
DiegoMD4 <diegolink4@hotmail.com>
2023-05-08 20:51:40 -0600

    Ticket index remake

--- resources/views/ticket/ticket_index.blade.php
+++ resources/views/ticket/ticket_index.blade.php
@@ -1,10 +1,30 @@
 @extends('layouts.app')
 @section('content')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+<script type="text/javascript">
+    setTimeout(function () {
+        $('#alert').alert('close');
+    }, 1325);
+</script>
 
-<div class="container" style="margin-top: 90px;">
+@if(Session::has('mensaje'))
+    <div id="alert" class="toast alert-dismissible fade show position-fixed bottom-0 end-0 p-3"
+         style="z-index: 11; margin: 10px; max-width: 100%; min-width: 200px;">
+        <div class="d-flex justify-content-between toast-header">
+            <strong class="me-auto">Notificación</strong>
+            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+        <div class="toast-body">
+            {{Session::get('mensaje')}}
+        </div>
+    </div>
+@endif
+
+
+<div class="container" style="margin-top: 90px;" >
   <div class="row">
     <div class="col">
-      <h4>Historial</h4>
+      <h1>Historial</h1>
     </div>
     <div class="col-md-auto">
       <form class="d-flex" action="{{ route('ticket.index') }}" method="GET">
@@ -22,64 +42,47 @@
     </div>
   </div>
 </div>
-<br>
 
-<div class="container-fluid">
-  <table class="responsivegene table caption-top">
-    <caption >Lista de tickets enviados por {{Auth::user()->name}}</caption>
-    <thead>
-      <tr>
-        <th>#</th>
-        <th>Descripción</th>
-        <th>Nombre de Usuario</th>
-        <th>Estado</th>
-        <th>Técnico Asignado</th>
-        <th>Área</th>
-        <th>Fecha de envío</th>
-        <th>Acción</th>
-      </tr>
-    </thead>
-    <tbody>
-      @forelse( $tickets as $ticket)
-       
-          <tr>
-            <td data-label="ID">{{ $ticket["id"] }}</td>
-            <td data-label="Descripción" class="desc">
-              {{ $ticket["descripcion"] }}</td>
-            <td data-label="Nombre">{{ $ticket->user->name}}</td>
-            <td data-label="Estado">{{ $ticket->estado->tipo_estado}}</td>
-            <td data-label="Tecnico Asignado">{{ $ticket["tecnico_asignado"] }}</td>
-            <td data-label="Area">{{ $ticket->user->area->nombre_area}}</td>
-            <td data-label="Fecha">{{ $ticket["created_at"] }}</td>
-            <td>
-              <div class="d-flex justify-content-between flex-wrap">
-                <a class="btn btn-warning mb-1 me-1 flex-grow-1" href="{{ url('/ticket/'.$ticket["id"].'/edit')}}">
-                  <i class="bi bi-pencil-fill"></i>Editar</a>
-                <form action="{{ url('/ticket/'.$ticket["id"]) }}" class="d-inline" method="POST">
-                  @csrf
-                  {{ method_field('DELETE') }}
-                  <button type="input" class="btn btn-danger mb-1 mx-1 flex-grow-1" type="submit" 
-                  onclick="return confirm('¿Desea eliminar este elemento?')" value="Borrar">
-                  <i class="bi bi-trash"></i>Borrar</button>
-                </form>
-                <a class="btn btn-info mb-1 ms-1 flex-grow-1" href="{{ route('ticket.show',$ticket->id) }}">
-                  <i class="bi bi-eye-fill"></i>Ver</a>
-              </div>
-            </td>
-          </tr>
-             
-          @empty
-          <tr>
-            <td colspan="8">
-              <h3 style="text-align: center">No se encontraron registros</h3>
-            </td>
-          </tr>
-      @endforelse
-    </tbody>
-  </table>
-  <div style="max-width: 50%">{!! $tickets->appends(['busqueda'=> $busqueda]) !!}</div>
+<div class="container-centered" style="margin-top: 30px">
+@forelse( $tickets as $ticket)
+    <div class="card">
+      <div class=" card-header d-flex justify-content-between">
+          <h4 class="card-tittle"><strong>ID de Ticket: {{$ticket["id"]}}</strong></h4>
+          <a class="btn btn-info btn-sm flex" href="{{ route('ticket.show',$ticket->id) }}">
+          <i class="bi bi-eye-fill"></i>Ver</a>
+      </div>
+      <div class="card-body">
+        
+        <strong>Descripción:</strong><p class="card-text">{{$ticket["descripcion"]}}</p>
+        <strong>Usuario: </strong>{{$ticket->user->name}}<br>
+        <strong>Estado: </strong>{{$ticket->tecnico_asignado}}<br>
+        <strong>Area: </strong>{{$ticket->user->area->nombre_area}}<br>
+        <strong>Fecha: </strong>{{$ticket["created_at"]}}<br>
+        <a class="btn btn-warning btn-sm" href="{{ url('/ticket/'.$ticket["id"].'/edit')}}">
+          <i class="bi bi-pencil-fill"></i>Editar</a>
+        <form action="{{ url('/ticket/'.$ticket["id"]) }}" class="d-inline" method="POST">
+          @csrf
+          {{ method_field('DELETE') }}
+          <button type="input" class="btn btn-danger btn-sm" type="submit" 
+          onclick="return confirm('¿Desea eliminar este elemento?')" value="Borrar">
+          <i class="bi bi-trash"></i>Borrar</button>
+          
+        </form>
+        
+      </div>
+    </div>
+    <br>
+    @empty
+    <br>
+    <div class="container" style="text-align: center">
+        <h3>No se encontraron registros</h3>
+    </div>
+    @endforelse
+  
+    <br>
+    <div style="max-width: 50%">{!! $tickets->appends(['busqueda'=> $busqueda]) !!}</div>
+  </div>
+  
 
-</div>
-                             
 
 @endsection
